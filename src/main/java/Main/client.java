f1f231b557780dd80d59bba7b79fdf6aa067cdc4
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-04-18 21:45:03 -0600

    Improved GUI, Annoying Java Hack, I dont know why the nimbus set look and feel changes the layout if i dont use it, that is why the boilerplate code is there, I will investigate it

--- src/main/java/Main/client.java
+++ src/main/java/Main/client.java
@@ -18,6 +18,22 @@ public class client {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GameView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
         GameView view = new GameView();
         GameController controller = new GameController(view);
         view.setController(controller);
